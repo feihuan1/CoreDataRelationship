b12f805ac1f5845dbbedbf81ecf7a6805ed98299
feihuan1 <ericrealtor1997@gmail.com>
2024-03-09 00:08:32 -0800

    Initial Commit

---
+++ CoreDataRelationship/CoreDataRelationshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataRelationshipApp.swift
+//  CoreDataRelationship
+//
+//  Created by Feihuan Peng on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataRelationshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
